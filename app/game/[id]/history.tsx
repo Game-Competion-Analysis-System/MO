f625abef455f97f114a1cc837a6b3d8fdd014065
bb <99488111+ggbb0711@users.noreply.github.com>
2026-02-02 14:00:53 +0700

    Add history

--- app/game/[id]/history.tsx
+++ app/game/[id]/history.tsx
@@ -1,6 +1,34 @@
+import HistorySections from "@/components/HistorySections";
 import { container, headers } from "@/constants/styles";
 import { FontAwesome6 } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const historySections = [
+  {
+    createdAtDate: "Dec 15, 2024",
+    createdAtTime: "14:32",
+    percentage: "99.8 %",
+    time: "0.8 s",
+    topPlayerName: "Tiểu Tiên Nữ",
+    eventName: "Year-End Festival",
+  },
+  {
+    createdAtDate: "Dec 14, 2024",
+    createdAtTime: "11:15",
+    percentage: "99.6 %",
+    time: "0.9 s",
+    topPlayerName: "Mộng Du Sinh",
+    eventName: "Guild Wars Season 6",
+  },
+  {
+    createdAtDate: "Dec 10, 2024",
+    createdAtTime: "10:22",
+    percentage: "99.0 %",
+    time: "1.2 s",
+    topPlayerName: "Vân Trung Tuyết",
+    eventName: "Thanksgiving Event",
+  },
+];
 
 export default function HistoryScreen() {
   return (
@@ -20,6 +48,7 @@ export default function HistoryScreen() {
           <FontAwesome6 name="arrow-down-short-wide" size={18} color="black" />
         </Pressable>
       </View>
+      <HistorySections historySections={historySections} />
     </View>
   );
 }
--- components/HistorySections.tsx
+++ components/HistorySections.tsx
@@ -1,7 +1,79 @@
+import { container, headers } from "@/constants/styles";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Octicons from "@expo/vector-icons/Octicons";
+import { StyleSheet, Text, View } from "react-native";
+
+interface History {
+  createdAtDate: string;
+  createdAtTime: string;
+  percentage: string;
+  time: string;
+  topPlayerName: string;
+  eventName: string;
+}
+
+function HistorySection({ section }: { section: History }) {
+  return (
+    <View style={styles.generalSectionInformation}>
+      <View style={styles.imgPreview}></View>
+      <View>
+        <View style={container.rowContainer}>
+          <Text style={headers.h2}>{section.createdAtDate}</Text>
+          <Text style={headers.h4}>{section.createdAtTime}</Text>
+          <Octicons
+            name="tracked-by-closed-completed"
+            size={14}
+            color="black"
+          />
+        </View>
+        <View style={container.rowContainer}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialCommunityIcons
+              name="lightning-bolt-outline"
+              size={14}
+              color="black"
+            />
+            <Text style={headers.h4}>{section.percentage}</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialIcons name="access-time" size={14} color="black" />
+            <Text style={headers.h4}>{section.time}</Text>
+          </View>
+          <Text style={headers.h4}>5 rankings</Text>
+        </View>
+        <Text style={headers.h4}>
+          Top: {section.topPlayerName} • {section.eventName}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
 export default function HistorySections({
-  history,
+  historySections,
 }: {
-  history: {
-    createdAt: string;
-  }[];
-}) {}
+  historySections: History[];
+}) {
+  return historySections.map((section, i) => (
+    <HistorySection key={i} section={section} />
+  ));
+}
+
+const styles = StyleSheet.create({
+  generalSectionInformation: {
+    padding: 12,
+    borderRadius: 12,
+    backgroundColor: "#ded5d37f",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 8,
+  },
+  imgPreview: {
+    width: 67,
+    height: 64,
+    borderRadius: 8,
+    backgroundColor: "purple",
+  },
+});
